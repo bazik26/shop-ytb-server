fe6ac89f6073671ed4440ab4f37e9742ee6c4f7e
bazik26 <bazikbazik26@gmail.com>
2025-02-22 11:45:32 +0300

    deploy2 cors fix

--- src/main.ts
+++ src/main.ts
@@ -49,7 +49,7 @@ async function bootstrap() {
 
   app.enableCors({
     credentials: true,
-    origin: ['http://localhost:3001', 'https://shop-ytb-client.onrender.com'],
+    origin: ['http://localhost:3001', 'https://cars-euro.com'],
   });
 
   const config = new DocumentBuilder()
